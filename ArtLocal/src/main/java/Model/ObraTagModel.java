46c881c3f8c952a50092ebc8a6dceafe8a819cf8
Lorenna Pires <lorenna.pires00@gmail.com>
2025-12-08 13:46:33 -0300

    classe model

---
+++ ArtLocal/src/main/java/Model/ObraTagModel.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class ObraTagModel {
+
+    private Integer idObra;
+    private Integer idTag;
+
+    public Integer getIdObra() {
+        return idObra;
+    }
+
+    public void setIdObra(Integer idObra) {
+        this.idObra = idObra;
+    }
+
+    public Integer getIdTag() {
+        return idTag;
+    }
+
+    public void setIdTag(Integer idTag) {
+        this.idTag = idTag;
+    }
+}
---
+++ ArtLocal/src/main/java/Model/TagModel.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class TagModel {
+
+    private Integer idTag;
+    private String nomeTag;
+
+    public Integer getIdTag() {
+        return idTag;
+    }
+
+    public void setIdTag(Integer idTag) {
+        this.idTag = idTag;
+    }
+
+    public String getNomeTag() {
+        return nomeTag;
+    }
+
+    public void setNomeTag(String nomeTag) {
+        this.nomeTag = nomeTag;
+    }
+}
